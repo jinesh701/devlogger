fba66ec59734fe788119dcf6d3cc94dc1bbf7aa7
Jinesh Patel <jinesh701@gmail.com>
2018-05-22 02:27:16 -0400

    changed logs component to get data via string interpolation using lifecycle method

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string;
+    text: string;
+    date: any;
+  }[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.logs = [
+      {
+        id: '1',
+        text: 'Generated components',
+        date: new Date('5/21/2018 12:54:23')
+      },
+      { id: '2', text: 'Added Bootstrap', date: new Date('5/22/2018 9:33:23') },
+      {
+        id: '3',
+        text: 'Added logs components',
+        date: new Date('5/22/2018 12:00:23')
+      }
+    ];
   }
-
 }
