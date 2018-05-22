a1abd69e28bdb4aba419dc4d823fecf011b876ff
Jinesh Patel <jinesh701@gmail.com>
2018-05-22 02:37:37 -0400

    adjusted to fetch data from log service

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,32 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Log } from '../../models/Log';
+
+import { LogService } from '../../services/log.service';
+
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-  logs: {
-    id: string;
-    text: string;
-    date: any;
-  }[];
+  logs: Log[];
 
-  constructor() {}
+  constructor(private logService: LogService) {}
 
   ngOnInit() {
-    this.logs = [
-      {
-        id: '1',
-        text: 'Generated components',
-        date: new Date('5/21/2018 12:54:23')
-      },
-      { id: '2', text: 'Added Bootstrap', date: new Date('5/22/2018 9:33:23') },
-      {
-        id: '3',
-        text: 'Added logs components',
-        date: new Date('5/22/2018 12:00:23')
-      }
-    ];
+    this.logs = this.logService.getLogs();
   }
 }
