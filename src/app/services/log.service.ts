89f8ee808a634cc7c9fe64ac0c60c51e0721b964
Jinesh Patel <jinesh701@gmail.com>
2018-06-20 20:04:58 -0400

    added stateSource BehaviorSubject and created clearState function

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -17,20 +17,25 @@ export class LogService {
   });
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() {
-    this.logs = [
-      {
-        id: '1',
-        text: 'Generated components',
-        date: new Date('5/21/2018 12:54:23')
-      },
-      { id: '2', text: 'Added Bootstrap', date: new Date('5/22/2018 9:33:23') },
-      {
-        id: '3',
-        text: 'Added logs components',
-        date: new Date('5/22/2018 12:00:23')
-      }
-    ];
+    // this.logs = [
+    //   {
+    //     id: '1',
+    //     text: 'Generated components',
+    //     date: new Date('5/21/2018 12:54:23')
+    //   },
+    //   { id: '2', text: 'Added Bootstrap', date: new Date('5/22/2018 9:33:23') },
+    //   {
+    //     id: '3',
+    //     text: 'Added logs components',
+    //     date: new Date('5/22/2018 12:00:23')
+    //   }
+    // ];
+
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
@@ -60,5 +65,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
   }
 }
